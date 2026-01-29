a9b73bbe34dc9a795ca04987d4ff1c3417b95425
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-01-29 23:03:42 +0900

    Title: 증가시키며 출력하기 3, Time: 91ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260129/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int cnt = 10;
+        while (cnt < 27) {
+            System.out.printf("%d ", cnt);
+            cnt += 2;
+        }
+    }
+}
