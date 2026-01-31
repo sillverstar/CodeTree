3c6c5e46e7603e04d0a64ecf581f084cac09359c
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-01-31 23:14:17 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 136ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260131/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[100];
+
+        for (int i = 0; i < 100; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i] == 0) {
+                int sum = arr[i - 3] + arr[i - 2] + arr[i - 1];
+                System.out.print(sum);
+                break;
+            }
+        }
+    }
+}
