3bfab620019fe00462edfbe3b0862a7b4e644a37
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-01-31 20:32:30 +0900

    Title: 원소 10개의 합, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260131/원소 10개의 합/sum-of-10-elements.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int cnt = 0;
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+            cnt += arr[i];
+        }
+        System.out.println(cnt);
+    }
+}
