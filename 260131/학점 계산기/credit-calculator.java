fe91c6984127ffd61ab4452634b8fbd1f576355d
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-01-31 21:51:54 +0900

    Title: 학점 계산기, Time: 159ms, Memory: 12MB, Status: Passed - Codetree

---
+++ 260131/학점 계산기/credit-calculator.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        double[] arr = new double[n];
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextDouble();
+            sum += arr[i];
+        }
+
+        double avg = sum / n;
+        
+        System.out.printf("%.1f%n", avg);
+
+        if (avg >= 4.0) {
+            System.out.print("Perfect");   
+        }
+        else if (avg >= 3.0) {
+            System.out.print("Good");
+        }
+        else {
+            System.out.print("Poor");
+        }
+
+        
+    }
+}
