5606019ab14248e7b980d016f92be8cd40052e5a
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-01-31 16:56:56 +0900

    Title: 감소시키며 출력하기 2, Time: 92ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260131/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 26;
+        while (a >= 10) {
+            System.out.printf("%d ", a);
+            a--;
+        }
+    }
+}
