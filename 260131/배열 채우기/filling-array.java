ffc291d0b36f5d084483a1aff7df262df24d0a09
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-01-31 22:00:31 +0900

    Title: 배열 채우기, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260131/배열 채우기/filling-array.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i] == 0) {
+                break;
+            }
+        }
+
+        for (int i = 9; i >= 0; i--) {
+            if (arr[i] != 0) {
+                System.out.printf("%d ", arr[i]);
+            }
+        }
+    }
+}
