031599a974f246bac25666b8dd6214887619405b
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-02-01 23:13:33 +0900

    Title: 문자 출력하기, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260201/문자 출력하기/print-text.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        char n = sc.next().charAt(0);
+
+        for (int i = 0; i < 8; i++) {
+            System.out.print(n);
+        }
+    }
+}
