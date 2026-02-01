c669f3a6e49190ac9f5537f939a8a26fb331dd1b
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-02-01 22:35:56 +0900

    Title: 나눗셈의 나머지, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260201/나눗셈의 나머지/remainder-of-division.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int[] remain = new int[b];
+
+        while (a > 1) {
+            int temp = a % b;
+            remain[temp] += 1;
+            a /= b;
+        }
+
+
+        System.out.print(Arrays.toString(remain));
+        int powsum = 0;
+        for (int i = 0; i < b; i++) {
+            powsum += Math.pow(remain[i], 2);
+        }
+        System.out.print(powsum);
+
+    }
+}
