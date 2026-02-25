b7907800b2294c693bb52bf6200683d87eeaf055
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-02-25 17:59:05 +0900

    Title: 폭탄 해제, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260225/폭탄 해제/bomb-removal.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Bomb {
+    String code;
+    char color;
+    int second;
+
+    public Bomb(String code, char color, int second) {
+        this.code = code;
+        this.color = color;
+        this.second = second;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String uCode = sc.next();
+        char lColor = sc.next().charAt(0);
+        int time = sc.nextInt();
+        // Please write your code here.
+
+        Bomb b = new Bomb(uCode, lColor, time);
+        System.out.println("code : " + b.code);
+        System.out.println("color : " + b.color);
+        System.out.println("second : " + b.second);
+    }
+}
