c256e4b03f9b4d673093e5e7cc068e1baea836b0
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-03-01 08:35:38 +0900

    Title: 비오는 날, Time: 147ms, Memory: 10MB, Status: Passed - Codetree

--- 260301/비오는 날/rainy-day.java
+++ 260301/비오는 날/rainy-day.java
@@ -19,25 +19,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int ansIdx = 0;
+
+        int ansIdx = -1;
         Predict[] arr = new Predict[n];
+
         for (int i = 0; i < n; i++) {
             String date = sc.next();
             String day = sc.next();
             String weather = sc.next();
             // Please write your code here.
             arr[i] = new Predict(date, day, weather);
-            if (weather.equals("Rain")) ansIdx = i;
-        }
 
-        for (int i = 0; i < n; i++) {
-            if (arr[i].weather.equals("Rain")) {
-                if (arr[i].date.compareTo(arr[ansIdx].date) < 0) {
+            if (weather.equals("Rain")) {
+                if (ansIdx == -1 || date.compareTo(arr[ansIdx].date) < 0) {
                     ansIdx = i;
                 }
             }
         }
-
         System.out.println(arr[ansIdx]);
     }
 }
