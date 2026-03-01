6c6e7052a1c3a176618a57c2422a9ee32af45b28
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-03-01 14:34:56 +0900

    Title: N개의 숫자 중 최소, Time: 170ms, Memory: 10MB, Status: Passed - Codetree

--- 260301/N개의 숫자 중 최소/min-of-n-num.java
+++ 260301/N개의 숫자 중 최소/min-of-n-num.java
@@ -10,21 +10,22 @@ public class Main {
             arr[i] = sc.nextInt();
         }
 
-        // 2. 최솟값 탐색
+        // 2. 최솟값 탐색 + 최솟값 개수 탐색
         int minVal = arr[0];
+        int cnt = 1;
         for (int i = 1; i < n; i++) {
+            // 최솟값 갱신
             if (minVal > arr[i]) {
                 minVal = arr[i];
+                cnt = 1; // 개수 초기화
             }
-        }
 
-        // 3. 최솟값 개수 탐색
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            if (minVal == arr[i]) {
+            // 최솟값 개수 갱신
+            else if (minVal == arr[i]) {
                 cnt++;
             }
         }
+
         System.out.println(minVal + " " + cnt);
     }
 }
