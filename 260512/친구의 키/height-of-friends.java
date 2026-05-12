ef08f1ba46f4f5deada13145c7fafc078eb57da2
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-05-12 10:25:05 +0900

    [Passed] 친구의 키 (727ms, 29MB)

---
+++ 260512/친구의 키/height-of-friends.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        // graph, indegree
+        List<Integer>[] graph = new ArrayList[N + 1];
+        for (int i = 0; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        int[] indegree = new int[N + 1];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+
+            graph[from].add(to);
+            indegree[to]++;
+        }
+
+
+        // queue
+        Queue<Integer> queue = new ArrayDeque<>();
+        
+        for (int i = 1; i <= N; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        // bfs
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+            
+            System.out.print(now + " ");
+
+            for (int next : graph[now]) {
+                indegree[next]--;
+
+                if (indegree[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
+
+    }
+}
